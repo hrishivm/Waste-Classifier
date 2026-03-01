28fcaba4e440dd23587eb7db872931fe329e489d
Hrishikesh V M <vmhrishikesh@gmail.com>
2026-03-01 22:48:57 +0530

    v1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ clf = WasteClassifier('waste_model_448.pkl')
 
 def main():
     print("--- Waste Classification CLI ---")
-    test_img = r'C:\Users\vmhri\OneDrive\Documents\Waste_Classifier\Samples\alum_1_pred.jpg'
+    test_img = r"D:\Desktop\stain_1.jpg"
     
     if os.path.exists(test_img):
         result = clf.predict(test_img)
